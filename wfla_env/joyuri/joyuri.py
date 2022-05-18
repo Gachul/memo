f246c31e5f7f959d83ffb052e7232e0ccf7eb2c1
Gachul <62504571+Gachul@users.noreply.github.com>
2022-05-18 17:59:41 +0900

    Add files via upload

--- wfla_env/joyuri/joyuri.py
+++ wfla_env/joyuri/joyuri.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder = 'static')
 
+@app.route("/category")
+def category_announcement():
+    return render_template("category.html")
+
 @app.route("/")
 def home():
     return render_template("home.html")

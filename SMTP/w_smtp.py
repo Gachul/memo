b28975dfb8708e844d8e2f2f0a3a166efab1f2fb
gachul <fgorunaway@gmail.com>
2022-05-19 15:57:54 +0900

    smtp update

--- SMTP/w_smtp.py
+++ SMTP/w_smtp.py
@@ -4,22 +4,34 @@ from string import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+
+_RECIEVER = 'phy1280@entrolink.com'
+
+with open('C:/Users/com/Desktop/Personal/resource/login.txt', 'r', encoding='utf-8') as f:
+    datas = f.readlines()
+    _EMAIL: str = datas[0].strip('\n')
+    _PASSWORD: str = datas[1].strip('\n')
     
-def practice():
+# 실행 확인
+def practice() -> None:
     recv_domain = 'imap.gmail.com'
     recv_port = 993
     
     send_domain = 'smtp.gmail.com'
-    send_port = 465
+    send_port = 587 #TLS  |  SSL=465
     
-    smtp = smtplib.SMTP(send_domain, send_port)
+    smtp = smtplib.SMTP_SSL(send_domain, 465)
     
-    smtp.ehlo()     # hello
-    smtp.starttls() # TLS 사용시 필요
-    smtp.login()
+    # smtp.ehlo()     # hello
+    # smtp.starttls() # TLS 사용시 필요
+    smtp.login(_EMAIL, _PASSWORD)
     
+    msg = MIMEText('SMTP Test')
+    msg['Subject'] = 'Title'
+    # msg['To'] = _RECIEVER
+    smtp.sendmail(_EMAIL, _RECIEVER, msg.as_string())
     
-    return 0
+    smtp.quit()
 
 class EmailHTMLContent:
     def __init__(self, str_subject, template, template_params):

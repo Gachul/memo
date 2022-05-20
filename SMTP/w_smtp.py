1e652ca724a6e472fbe52ed25c08b1feb6b6d41d
gachul <fgorunaway@gmail.com>
2022-05-20 10:48:47 +0900

    addon

--- SMTP/w_smtp.py
+++ SMTP/w_smtp.py
@@ -5,7 +5,16 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
+# instead Final Variable
 _RECIEVER = 'phy1280@entrolink.com'
+
+RECV_DOMAIN = 'imap.gmail.com'
+RECV_PORT = 993
+    
+SEND_DOMAIN = 'smtp.gmail.com'
+SEND_PORT = 587 #TLS  |  SSL=465
+
+
 
 with open('C:/Users/com/Desktop/Personal/resource/login.txt', 'r', encoding='utf-8') as f:
     datas = f.readlines()
@@ -14,13 +23,8 @@ with open('C:/Users/com/Desktop/Personal/resource/login.txt', 'r', encoding='utf
     
 # 실행 확인
 def practice() -> None:
-    recv_domain = 'imap.gmail.com'
-    recv_port = 993
     
-    send_domain = 'smtp.gmail.com'
-    send_port = 587 #TLS  |  SSL=465
-    
-    smtp = smtplib.SMTP_SSL(send_domain, 465)
+    smtp = smtplib.SMTP_SSL(SEND_DOMAIN, 465)
     
     # smtp.ehlo()     # hello
     # smtp.starttls() # TLS 사용시 필요
@@ -32,16 +36,21 @@ def practice() -> None:
     smtp.sendmail(_EMAIL, _RECIEVER, msg.as_string())
     
     smtp.quit()
+    
+def Other_SMTP() -> None:
+    
+    smtp = smtplib.SMTP(SEND_DOMAIN, SEND_PORT)
+    
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(_EMAIL, _PASSWORD)
+    
+    msg = MIMEText('SMTP Test')
+    msg['Subject'] = 'Test'
+    msg ['To'] = _RECIEVER
 
-class EmailHTMLContent:
-    def __init__(self, str_subject, template, template_params):
-        assert isinstance(template, Template)
-        assert isinstance(template_params, dict)
-        
-        self.msg = MIMEMultipart()
-        
-        # Email Title
-        self.msg['Subject'] = str_subject
-        
-        # Email Content
-        str_msg = template.safe_substitute(**template_params)
+    smtp.sendmail(_EMAIL, _RECIEVER, msg.as_string())
+    
+    smtp.quit()
+    
+Other_SMTP()
---
+++ memonote/use_function/use_function.py
@@ -0,0 +1,44 @@
+# just print function
+# return = None
+# in java's void function
+def test01() -> None:
+    print('hello')
+
+# This function is return value is 10
+# value = test02()
+# is value = test02 function's return value
+# value = 10    
+def test02() -> int:
+    return 10
+
+# This function converts the received values into string types
+# And return value's type is string
+# test03 is received two values
+# a is int type, b is string type
+def test03(a: int, b: str) -> str:
+    return f'input int value is {a}, string value is {b}'
+
+# Received values are used to plus arithmetic
+# So, Return value type is int
+def test04(a: int, b: int) -> int:
+    return f'{a} + {b} = {a + b}'
+
+# lambda is one-line function
+# [value1] if [conditional expression] else [value2]
+# => if([conditional expression]): value1
+#    else: value2
+test05 = lambda a, b, c : b if a > 10 else c
+
+# test06 and test05 are the same
+def test06(a: int, b: int, c: int) -> int:
+    if(a > 10):
+        return b
+    else:
+        return c
+
+test01()
+val02 = test02(); print(val02)
+val03 = test03(10, "choice"); print(val03)
+val04 = test04(25, 50); print(val04)
+val05 = test05(5,10,15); print(val05)
+val06 = test06(5,10,15); print(val06)

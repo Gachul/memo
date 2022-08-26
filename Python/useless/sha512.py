e8e2ef7725cd2af1412d72b1584fbc7d411ee878
wizone <wizone@joyuri.com>
2022-08-26 12:05:09 +0900

    qwasd

--- Python/useless/sha512.py
+++ Python/useless/sha512.py
@@ -1,7 +1,6 @@
 from binascii import unhexlify as unhex
 from binascii import hexlify as hex
-import hashlib
-import passlib
+from passlib.hash import sha512_crypt as sh512
 
 # init datas
 _password = 'dnflskfk'
@@ -10,6 +9,14 @@ _hash_type = 'SHA-512'
 _type_512_header = '$6$'
 
 # main sentence
-aaa= passlib.hash.sha512_crypt(_password)
+aaa = sh512.hash(_password)
+
+# $6$rounds$salt$hash
+bbb = sh512.using(rounds=1500, salt ='salt').hash(_password)
+
+# if rounds=5000 >> $6$salt$hash
+ccc = sh512.using(rounds=5000, salt = 'salt').hash(_password)
 
 print(aaa)
+print(bbb)
+print(ccc)

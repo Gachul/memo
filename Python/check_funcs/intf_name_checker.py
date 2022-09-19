cb6c75a70b57851131e8c47ff5e1a48944e5da68
wizone <wizone@joyuri.com>
2022-09-19 17:05:42 +0900

    220919

--- Python/check_funcs/intf_name_checker.py
+++ Python/check_funcs/intf_name_checker.py
@@ -6,14 +6,14 @@ _INTERFACE_NAME = sys.argv[2]
 
 def normal_interface(intf):
     if(intf[0:3] != "eth"):
-        if(intf[:5] == "bond0"):
+        if(intf == "bond0"):
             return 1
         return 0
     try:
-        interface_number = int(intf[3:])    # check interface number is valid
+        check_number = intf[3:]
+        interface_number = int(check_number)    # check interface number is valid
 
         inter_number = str(interface_number)
-        check_number = intf[3:]
 
         if(inter_number != check_number):
             return 0
@@ -28,20 +28,42 @@ def alias_interface(intf):
     if(intf[0:3] != "eth"):
         return 0
     try:
-        interface_number = int(intf[3])
-        if(interface_number > 20):
-            return 0
+        if(intf[4] == ":"):
+            interface_number = int(intf[3])
+            if(interface_number > 10):
+                return 0
 
-        if(intf[4] != ":"):
-            return 0
+            interface_mask = int(intf[5:])
 
-        interface_mask = int(intf[5:])
+            inter_mask = str(interface_mask)
+            check_mask = intf[5:]
+            
+            if(inter_mask != check_mask ):
+                return 0
+            if(interface_mask > 255):
+                return 0
+            
+        elif(intf[5] == ":"):
+            chk_intf = intf[3:5]
+            interface_number = int(chk_intf)
+            
+            if(interface_number > 20):
+                return 0
+            
+            if(chk_intf != str(interface_number)):
+                return 0
 
-        inter_mask = str(interface_mask)
-        check_mask = intf[5:]
-        if(inter_mask != check_mask ):
-            return 0
-        if(interface_mask > 255):
+            interface_mask = int(intf[6:])
+
+            inter_mask = str(interface_mask)
+            check_mask = intf[6:]
+            
+            if(inter_mask != check_mask ):
+                return 0
+            if(interface_mask > 255):
+                return 0
+            
+        else:
             return 0
 
         return 1
@@ -52,47 +74,53 @@ def vlan_interface(intf):
     if(intf[0:3] != "eth"):
         return 0
     try:
-        interface_number = int(intf[3])
-        if(interface_number > 20):
-            return 0
+        if(intf[4] == "."):
+            interface_number = int(intf[3])
+            if(interface_number > 10):
+                return 0
 
-        if(intf[4] != "."):
-            return 0
+            interface_mask = int(intf[5:])
 
-        interface_mask = int(intf[5:])
+            inter_mask = str(interface_mask)
+            check_mask = intf[5:]
+            
+            if(inter_mask != check_mask ):
+                return 0
+            if(interface_mask > 255):
+                return 0
+            
+        elif(intf[5] == "."):
+            chk_intf = intf[3:5]
+            interface_number = int(chk_intf)
+            
+            if(interface_number > 20):
+                return 0
+            
+            if(chk_intf != str(interface_number)):
+                return 0
 
-        inter_mask = str(interface_mask)
-        check_mask = intf[5:]
+            interface_mask = int(intf[6:])
 
-        if(inter_mask != check_mask ):
-            return 0
-        if(interface_mask > 255):
+            inter_mask = str(interface_mask)
+            check_mask = intf[6:]
+            
+            if(inter_mask != check_mask ):
+                return 0
+            if(interface_mask > 255):
+                return 0
+            
+        else:
             return 0
 
         return 1
-
     except:
         return 0
 
 def bonding_interface(intf):
-    if(intf[:5] != "bond0"):
+    if(intf != "bond0"):
         return 0
-    try:
-        interface_number = int(intf[4:])
-
-        inter_number = str(interface_number)
-        check_number = intf[4:]
-
-        if(inter_number != check_number):
-            return 0
-
-        if(interface_number > 5):
-            return 0
-
-        return 1
-
-    except:
-        return 0
+    
+    return 1
 
 def bridge_interface(intf):
     if(intf != "br0"):
@@ -120,7 +148,5 @@ def mode_checker(mode, interface):
         os.system("echo 1")
     else:
         os.system("echo 0")
-print(_MODE)
-print(_INTERFACE_NAME)
+
 mode_checker(_MODE, _INTERFACE_NAME)
-        

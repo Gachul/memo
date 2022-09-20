e580b79cfd8207a1b73b28711b46bc1dfc220ea9
wizone <wizone@joyuri.com>
2022-09-20 17:57:08 +0900

    220920

--- Python/check_funcs/ip_checker.py
+++ Python/check_funcs/ip_checker.py
@@ -1,4 +1,7 @@
 import sys, os
+import string
+
+_IPV6_SET = string.digits + 'abcdef'
 
 _VALID = 0
 _TYPE_CHECK = sys.argv[1]
@@ -7,6 +10,9 @@ _ORG_INTERFACE = sys.argv[2]
 
 def ipv4(ip, subnet):
     try:
+        if(int(subnet) > 32):
+            return 0
+        
         ip_devide = ip.split('.')
         
         if(len(ip_devide) != 4):
@@ -19,11 +25,29 @@ def ipv4(ip, subnet):
                     return 0
     except:
         return 0
+    
     return 1
 
 def ipv6(ip, prefix):
     
-    return 0
+    try:
+        if(int(prefix) > 128):
+            return 0
+        
+        ip_devide = ip.split(':')
+        
+        if(len(ip_devide) != 8):
+            return 0
+        else:
+            for part_ip in ip_devide:
+                
+                for part in part_ip:
+                    if(part not in _IPV6_SET):
+                        return 0
+    except:
+        return 0
+    
+    return 1
 
 def devider(all_ip):
     ip_box = all_ip.split('/')

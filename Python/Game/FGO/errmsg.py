234394d849ec6d647d8e7f7a25b3fd189ed81a6d
wizone <wizone@joyuri.com>
2022-10-11 14:28:42 +0900

    del

--- Python/Game/FGO/errmsg.py
+++
@@ -1,7 +0,0 @@
-# Error Message
-
-_invalid_date_select = "Please select valid date."
-
-def err_msg(err_count):
-    if(err_count == 1):
-        return _invalid_date_select
--- Python/Game/FGO/sample.py
+++
@@ -1,4 +0,0 @@
-# This is just add to github
-# This File purpose is FGO Stone Calculator
-# Thx
-

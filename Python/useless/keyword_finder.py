e4cfedd487584b32bab81b50086c66d87bc7b09c
Gachul <62504571+Gachul@users.noreply.github.com>
2022-11-22 15:32:08 +0900

    Delete Python directory

--- Python/useless/keyword_finder.py
+++
@@ -1,5 +0,0 @@
-import keyword_saver as ksv
-
-print(ksv._MAIN)
-
-ksv.show_string('Apple')
--- Python/useless/keyword_saver.py
+++
@@ -1,9 +0,0 @@
-_MAIN='''
-This is Keyword Finder
-But This Version is Prototype
-Thank You!
-'''
-
-def show_string(string):
-    print(string)
-    
